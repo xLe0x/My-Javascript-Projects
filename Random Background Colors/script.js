89e37bb5be28f04055173b23e9b7c373ecf5ad71
xLe0x <ammarmosaber@gmail.com>
2023-01-02 14:57:04 +0200

    initial

--- Random Background Colors/script.js
+++ Random Background Colors/script.js
@@ -1,5 +1,6 @@
 const hexDigits = ["a", "b", "c", "d", "e", "f", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+function generateColor() {
 let hexCode = "#";
 for (let i = 0; i < 6; i++) {
   hexCode += hexDigits[Math.floor(Math.random() * hexDigits.length)];
@@ -8,3 +9,6 @@ for (let i = 0; i < 6; i++) {
 document.querySelector("h1").innerHTML += hexCode;
 
 document.body.style.backgroundColor = hexCode;
+}
+
+setInterval(generateColor, 2000);
